705220fb00d86cc27ff30bba350b18318b2608ce
Bach Nguyen <bachnv@teca.vn>
2025-11-19 20:24:53 +0700

    new features

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -232,6 +232,8 @@ func loadMenu() map[string]interface{} {
 func loadIntro(extras map[string]interface{}) map[string]interface{} {
 	builder := strings.Builder{}
 
+	builder.WriteString("<b>Hãy ra lệnh cho tôi!</b>")
+	builder.WriteString("\n")
 	builder.WriteString("<b>Tra cứu và cảnh báo giá vàng</b>\n")
 	builder.WriteString("\n")
 	builder.WriteString("<code><b>/gold_live</b></code> - <i>tra cứu giá vàng mới nhất</i>\n")
@@ -242,8 +244,6 @@ func loadIntro(extras map[string]interface{}) map[string]interface{} {
 	builder.WriteString("\n")
 	builder.WriteString("<code><b>/feedback</b></code> - <i>gửi góp ý cải thiện bot</i>\n")
 	builder.WriteString("<code><b>/donate</b></code> - <i>☕︎ give me a coffee cup</i>\n")
-	builder.WriteString("\n")
-	builder.WriteString("<b>Hãy ra lệnh cho tôi!</b> 🐕")
 
 	intro := map[string]interface{}{
 		"parse_mode": "HTML",
