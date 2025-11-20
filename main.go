afc439edc358a0a54afc755105a1374277557fdf
Bach Nguyen <bachnv@teca.vn>
2025-11-20 17:25:04 +0700

    new features

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/mrgold/internal/crawler"
 	"github.com/mrgold/internal/googlesheet"
 	"github.com/mrgold/internal/httpclient"
 	"github.com/mrgold/internal/httpserver"
@@ -38,10 +37,10 @@ func main() {
 		}),
 	)
 
-	c := crawler.NewCrawler(
-		crawler.WithHTTPClient(httpClient),
-		crawler.WithStore(s),
-	)
+	//c := crawler.NewCrawler(
+	//	crawler.WithHTTPClient(httpClient),
+	//	crawler.WithStore(s),
+	//)
 
 	telegramClient := telegram.NewClient(
 		telegram.WithHTTPClient(httpClient),
@@ -84,7 +83,7 @@ func main() {
 		defer wg.Done()
 
 		for {
-			c.Crawl()
+			//c.Crawl()
 
 			select {
 			case <-ctx.Done():
