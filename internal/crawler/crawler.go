35730852480e860f98c0d95589cc97f869963237
Bach Nguyen <bachnv@teca.vn>
2025-12-05 15:30:17 +0700

    new features

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -248,10 +248,10 @@ func (c *Crawler) crawlDOJI() {
 		var golds []store.Gold
 
 		idMatrix := map[string]string{
-			"AVPL/SJC":                          "01",
+			"AVPL/SJC": "01",
 			"Nhẫn tròn 9999 (Hưng Thịnh Vượng)": "02",
-			"Nữ trang 9999":                     "03",
-			"Nữ trang 999":                      "04",
+			"Nữ trang 9999": "03",
+			"Nữ trang 999":  "04",
 		}
 
 		doc.Find("table.goldprice-view tbody tr").Each(func(_ int, s *goquery.Selection) {
@@ -317,10 +317,10 @@ func (c *Crawler) crawlDOJIV2() {
 		}
 
 		goldCodes := map[string]string{
-			"AVPL/SJC":                          "doji01",
+			"AVPL/SJC": "doji01",
 			"Nhẫn tròn 9999 (Hưng Thịnh Vượng)": "doji02",
-			"Nữ trang 9999":                     "doji03",
-			"Nữ trang 999":                      "doji04",
+			"Nữ trang 9999": "doji03",
+			"Nữ trang 999":  "doji04",
 		}
 
 		goldIDs := c.stg.GetGoldIDs("doji")
